cf85fdcfe8ce8e89f61818ff4e24252fbfd610e8
Zuberator <przemyslaw.zubik@outlook.com>
2022-08-25 14:07:50 +0200

    Adding js code to handle alert and button function

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,45 @@
 let alertButton = document.getElementById("alert-button");
+let resetButton = document.getElementById("reset-button");
 let alertWindow = document.getElementById("alert");
 let overlay = document.getElementById("overlay");
 let xIcon = document.getElementById("x-icon");
+let counter = document.getElementById("counter");
+let buttonClickCounter = 0;
+
+function checkButton(times) {
+    counter.textContent = times + " times";
+    if (times > 5) return resetButton.style.display = "block"
+    return resetButton.style.display = "none";
+};
 
 alertButton.addEventListener('click', () => {
-    alertWindow.style.display = "block";
+    alertWindow.style.display = "flex";
     overlay.style.display = "block";
-    // document.body.classList.add("dimm");
+    buttonClickCounter += 1;
+    checkButton(buttonClickCounter);
+});
+
+resetButton.addEventListener('click', () => {
+    buttonClickCounter = 0;
+    checkButton(buttonClickCounter);
 });
 
 xIcon.addEventListener('click', () => {
     alertWindow.style.display = "none";
     overlay.style.display = "none";
-    // document.body.classList.remove("dimm");
 });
+
+document.addEventListener('mouseup', (element) => {
+    if (alertWindow.style.display = "flex") {
+        if (!alertWindow.contains(element.target)) {
+            alertWindow.style.display = 'none';
+            overlay.style.display = "none";
+        }
+    }
+  });
+
+
+
+
+
+
