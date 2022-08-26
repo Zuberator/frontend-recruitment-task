01e4722e18332be279f231a368768f85415f0631
Zuberator <przemyslaw.zubik@outlook.com>
2022-08-26 18:31:44 +0200

    Added comments and storage, code cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,41 @@
+// VARIABLES
 let alertButton = document.getElementById("alert-button");
 let resetButton = document.getElementById("reset-button");
 let alertWindow = document.getElementById("alert");
 let overlay = document.getElementById("overlay");
 let xIcon = document.getElementById("x-icon");
 let counter = document.getElementById("counter");
-let buttonClickCounter = 0;
+let buttonClickCounter = localStorage.getItem("counter");
 
+// CHECK, THEN WRITE COUNTER STATE, SHOW/HIDE RESET BUTTON
 function checkButton(times) {
     counter.textContent = times + " times";
+    localStorage.setItem("counter", times);
     if (times > 5) return resetButton.style.display = "block"
     return resetButton.style.display = "none";
 };
 
+// ALERT BUTTON CLICK LOGIC - COUNTER
 alertButton.addEventListener('click', () => {
     alertWindow.style.display = "flex";
     overlay.style.display = "block";
-    buttonClickCounter += 1;
+    buttonClickCounter ++;
     checkButton(buttonClickCounter);
 });
 
+// RESET BUTTON CLICK LOGIC
 resetButton.addEventListener('click', () => {
     buttonClickCounter = 0;
     checkButton(buttonClickCounter);
 });
 
+// CLOSE ALERT MODAL ONCLICK X-ICON
 xIcon.addEventListener('click', () => {
     alertWindow.style.display = "none";
     overlay.style.display = "none";
 });
 
+// CLOSE ALERT MODAL ONCLICK OUTSIDE
 document.addEventListener('mouseup', (element) => {
     if (alertWindow.style.display = "flex") {
         if (!alertWindow.contains(element.target)) {
@@ -36,10 +43,4 @@ document.addEventListener('mouseup', (element) => {
             overlay.style.display = "none";
         }
     }
-  });
-
-
-
-
-
-
+});
